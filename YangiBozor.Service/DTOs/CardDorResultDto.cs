9c30d4032c7f5733386742e9e301581f7c1ecc83
AlJavhar <My274737@gmail.com>
2023-04-08 12:52:50 +0500

    Finished my issue! Created Card, Order, Product, User Dtos :)

---
+++ YangiBozor.Service/DTOs/CardDorResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace YangiBozor.Service.DTOs;
+
+public class CardDorResultDto : Auditable
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
