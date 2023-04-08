aaf5847751ca71abb0813fc076dc22f7ea36701c
AlJavhar <My274737@gmail.com>
2023-04-08 12:32:03 +0500

    Created Order, User Dtos

---
+++ YangiBozor.Service/DTOs/OrderForResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace YangiBozor.Service.DTOs;
+
+public class OrderForResultDto : Auditable
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public OrderStatus Status { get; set; }
+    public decimal TotalMoneyToPaid { get; set; }
+    public bool IsPaid { get; set; } = false;
+}
--- YangiBozor.Service/DTOs/UserForResultDto.cs
+++ YangiBozor.Service/DTOs/UserForResultDto.cs
@@ -1,11 +1,10 @@
 ﻿namespace YangiBozor.Service.DTOs;
 
-public class UserForCreationDto
+public class UserForResultDto : Auditable
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Phone { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
     public UserRole Role { get; set; }
 }
